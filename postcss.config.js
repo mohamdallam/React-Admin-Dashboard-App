487ca391cee1bc1c4b34d4f262d91982b6e336bd
mohamdallam <mohamedahmedallam1@gmail.com>
2022-06-01 16:23:53 +0200

    Init

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+};
